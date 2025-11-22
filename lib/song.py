59302e184c374e8ba56dc8ee99df68e27092668a
Patrickson2 <thairupatrickson@gmail.com>
2025-11-23 01:07:52 +0300

    made a song adding to collection lists

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,32 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Count total songs
+        Song.count += 1
+
+        # Track genres
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+
+        # Track artists
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+
+        # Count songs by genre
+        if genre not in Song.genre_count:
+            Song.genre_count[genre] = 0
+        Song.genre_count[genre] += 1
+
+        # Count songs by artist
+        if artist not in Song.artist_count:
+            Song.artist_count[artist] = 0
+        Song.artist_count[artist] += 1
